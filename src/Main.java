433ec4b8836032297f8ca157764582f2163213b9
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-02-04 23:48:12 +0530

    Adding new classes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer();
-        System.out.println(customer.getCustomerName()+" "+customer.getCustomerEmail()+" "
-                +customer.getCreditLimit());
-        
+        Customer customer1 = new Customer();
+        System.out.println(customer1.getCustomerName()+" "+customer1.getCustomerEmail()+" "
+                +customer1.getCreditLimit());
+        Customer customer2 = new Customer("Bheem","bheem@rrr.com","20000000");
+        System.out.println(customer2.getCustomerName()+" "+customer2.getCustomerEmail()+" "
+                +customer2.getCreditLimit());
+        Customer customer3 = new Customer("Raju","raju@rrr.com");
+        System.out.println(customer3.getCustomerName()+" "+customer3.getCustomerEmail()+" "
+                +customer3.getCreditLimit());
+
     }
 }
