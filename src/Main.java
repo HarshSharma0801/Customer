f91ec0711dc1d99471fb1e12de66b2b0c2336872
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-02-05 23:34:19 +0530

    Adding new objects

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,10 @@ public class Main {
         Customer customer3 = new Customer("Raju","raju@rrr.com");
         System.out.println(customer3.getCustomerName()+" "+customer3.getCustomerEmail()+" "
                 +customer3.getCreditLimit());
+        Customer customer4 = new Customer("Harsh","Harsh@Best.com",
+                "unlimited");
+        System.out.println(customer4.getCustomerName()+" "+customer4.getCustomerEmail()+" "
+                +customer4.getCreditLimit());
 
     }
 }
